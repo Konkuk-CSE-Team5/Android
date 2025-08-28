115c7141c658946ce0e75960ffb238f6c75e0181
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-08-29 00:47:30 +0900

    feat: success 필드 제거

--- app/src/main/java/com/konkuk/hackathon/core/network/response/base/BaseResponse.kt
+++ app/src/main/java/com/konkuk/hackathon/core/network/response/base/BaseResponse.kt
@@ -5,7 +5,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class BaseResponse<T>(
-    @SerialName("success") val success: Boolean,
     @SerialName("code") val code: Int,
     @SerialName("message") val message: String,
     @SerialName("data") val data: T,
