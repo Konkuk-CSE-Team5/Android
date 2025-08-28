af712016901f0e7afd4e7e4c108d5b5872b36ede
Chae Minji <162430621+alswlekk@users.noreply.github.com>
2025-08-28 09:12:08 +0900

    Feat: 센터 설정 부분 네비게이션 연결 및 둥근 부분만 클릭되도록 수정

--- app/src/main/java/com/konkuk/hackathon/core/navigation/center/CenterRoute.kt
+++ app/src/main/java/com/konkuk/hackathon/core/navigation/center/CenterRoute.kt
@@ -3,7 +3,8 @@ package com.konkuk.hackathon.core.navigation.center
 import kotlinx.serialization.Serializable
 
 sealed interface CenterRoute {
-
+    @Serializable
+    data object CenterInfoModify : CenterRoute
 }
 
 sealed interface CenterTabRoute : CenterRoute {
