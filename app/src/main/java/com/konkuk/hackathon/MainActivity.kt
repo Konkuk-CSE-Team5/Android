8d67af6198ba2d8b87360732d1c4014e0e50100a
Chae Minji <162430621+alswlekk@users.noreply.github.com>
2025-08-28 18:21:18 +0900

    Feat : 알림 권한 요청하는 부분 추가

--- app/src/main/java/com/konkuk/hackathon/MainActivity.kt
+++ app/src/main/java/com/konkuk/hackathon/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.konkuk.hackathon
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.konkuk.hackathon.core.designsystem.theme.OnItTheme
 import com.konkuk.hackathon.core.navigation.MainNavHost
 import com.konkuk.hackathon.core.navigation.MainTab
@@ -22,6 +30,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OnItTheme {
+                val context = LocalContext.current
+
+                val permissionLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = { isGranted: Boolean ->
+                        if (isGranted) {
+                            // 권한이 허용되었을 때의 로직
+                            println("알림 권한이 허용되었습니다.")
+                        } else {
+                            // 권한이 거부되었을 때의 로직
+                            println("알림 권한이 거부되었습니다.")
+                        }
+                    }
+                )
+                LaunchedEffect(Unit) {
+                    // Android 13 (API 33) 이상에서만 실행
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        // 이미 권한이 있는지 확인
+                        val checkPermission = ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.POST_NOTIFICATIONS
+                        )
+                        // 권한이 없다면 요청
+                        if (checkPermission != PackageManager.PERMISSION_GRANTED) {
+                            permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                        }
+                    }
+                }
+
                 val mainNavigator = rememberMainNavigator()
 
                 Scaffold(
